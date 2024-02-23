6ce18cb0349009f6aa91f80a1298f37189e7950b
KrLite <68179735+KrLite@users.noreply.github.com>
2024-02-23 23:14:13 +0800

    hooray

---
+++ src/main/java/net/krlite/reicollapsibleentries/config/modmenu/REICollapsibleEntriesConfigScreen.java
@@ -0,0 +1,42 @@
+package net.krlite.reicollapsibleentries.config.modmenu;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.krlite.reicollapsibleentries.REICollapsibleEntries;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+import java.util.ArrayList;
+
+public class REICollapsibleEntriesConfigScreen {
+    private final ConfigBuilder configBuilder = ConfigBuilder.create()
+            .setTitle(Text.translatable("screen.reicollapsibleentries.config.title"))
+            .transparentBackground()
+            .setShouldListSmoothScroll(true)
+            .setSavingRunnable(REICollapsibleEntries.CONFIG_HOLDER::save);
+    private final ConfigEntryBuilder entryBuilder = configBuilder.entryBuilder();
+
+    public REICollapsibleEntriesConfigScreen(Screen parent) {
+        configBuilder.setParentScreen(parent);
+
+        initEntries();
+    }
+
+    public Screen build() {
+        return configBuilder.build();
+    }
+
+    private void initEntries() {
+        ConfigCategory category = configBuilder.getOrCreateCategory(Text.empty());
+
+        category.addEntry(entryBuilder.startStrList(
+                                Text.translatable("config.reicollapsibleentries.custom_tags"),
+                                REICollapsibleEntries.CONFIG.customTags
+                        )
+                        .setExpanded(true)
+                        .setSaveConsumer(value -> REICollapsibleEntries.CONFIG.customTags = new ArrayList<>(value))
+                        .build()
+        );
+    }
+}
--- src/main/java/net/krlite/reicollapsibleentries/config/modmenu/REICollapsibleEntriesModMenuIntegration.java
+++ src/main/java/net/krlite/reicollapsibleentries/config/modmenu/REICollapsibleEntriesModMenuIntegration.java
@@ -1,12 +1,13 @@
-package net.krlite.reicollapsibleentries.config;
+package net.krlite.reicollapsibleentries.config.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import net.krlite.reicollapsibleentries.config.REICollapsibleEntriesConfig;
 
 public class REICollapsibleEntriesModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(REICollapsibleEntriesConfig.class, parent).get();
+        return parent -> new REICollapsibleEntriesConfigScreen(parent).build();
     }
 }
