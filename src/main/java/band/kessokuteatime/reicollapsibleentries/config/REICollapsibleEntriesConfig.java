f7d8e5553f8b76ff94e39ace25b70dc14c41366f
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-18 08:10:04 +0800

    update

--- src/main/java/band/kessokuteatime/reicollapsibleentries/config/REICollapsibleEntriesConfig.java
+++ src/main/java/band/kessokuteatime/reicollapsibleentries/config/REICollapsibleEntriesConfig.java
@@ -1,12 +1,21 @@
 package band.kessokuteatime.reicollapsibleentries.config;
 
+import com.electronwill.nightconfig.core.serde.annotations.SerdeComment;
+import com.electronwill.nightconfig.core.serde.annotations.SerdeDefault;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 import java.util.ArrayList;
+import java.util.function.Supplier;
 
 @Config(name = "reicollapsibleentries")
 @Config.Gui.Background(Config.Gui.Background.TRANSPARENT)
 public class REICollapsibleEntriesConfig implements ConfigData {
-    public ArrayList<String> customTags = new ArrayList<>();
+    @ConfigEntry.Gui.Excluded
+    private transient final Supplier<ArrayList<String>> customTagsProvider = ArrayList::new;
+
+    @SerdeDefault(provider = "customTagsProvider")
+    @SerdeComment("Define")
+    public ArrayList<String> customTags = customTagsProvider.get();
 }
